3d226c94cc2fc6d17b21b4a344b7a4decd7aa635
j-kingz <justin.macharia@strathmore.edu>
2024-05-21 12:38:48 +0300

    Upgrade

---
+++ in.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+echo 'This is my first php code ';
+
+print '<br>';
+
+print "Today is <span style = 'color: green;'?>Tuesday</span>";
+
+print '<br>';
+
+print "44566";
+
+print '<br>';
+
+print date ('1');
+
+print '<br>';
+
+print "8+6";
+
+print '<br>';
+
+print date('1');
+
+print '<br>';
+
+print date ('1',strtotime('+5months'));
+
+print '<br>';
+
+print date ('1, jS-F-Y', strtotime('+3days'));
+
+print '<br>';
+
+
+?> 
+</body>
+</html>
+
